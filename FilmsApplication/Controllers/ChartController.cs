670f154eff713c2e08095a733c1e8cb699866fc2
Irina Bespalova <irinabespalova427@gmail.com>
2023-03-19 00:51:49 +0100

    change for stage: 1.4-1.5

---
+++ FilmsApplication/Controllers/ChartController.cs
@@ -0,0 +1,47 @@
+﻿using FilmsApplication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmsApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChartController : ControllerBase
+    {
+        private readonly DbfilmsContext _context;
+
+        public ChartController(DbfilmsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("JsonDataCountry")]
+        public JsonResult JsonDataCountry()
+        {
+            var countries = _context.Countries.Include(b => b.Actors).ToList();
+            List<object> countryActor = new();
+            countryActor.Add(new[] { "Country", "Number of actors" });
+            foreach (var coutries in countries)
+            {
+                countryActor.Add(new object[] { coutries.CountryName, coutries.Actors.Count });
+            }
+
+            return new JsonResult(countryActor);
+        }
+
+        [HttpGet("JsonDataGenre")]
+        public JsonResult JsonDataGenre()
+        {
+            var genres = _context.Genres.Include(m => m.FilmGenres).ToList();
+            List<object> genreFilm = new();
+            genreFilm.Add(new[] { "Genre", "Number of films:" });
+            foreach (var genre in genres)
+            {
+                genreFilm.Add(new object[] { genre.GenreName, genre.FilmGenres.Count });
+            }
+
+            return new JsonResult(genreFilm);
+        }
+    }
+}
